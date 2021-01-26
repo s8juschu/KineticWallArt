19f9c0e5dbba7bf4bd2d10d748ccebb76ad15654
Julia Schulz <s8juschu@stud.uni-saarland.de>
2021-01-26 15:53:44 +0100

    Fix modals, pass unique id and display rotation changes in frontend

--- kineticWallArt/website/views.py
+++ kineticWallArt/website/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import reverse, render
 
 
 def index(request):
-    return render(request, 'index.html')
+    elements = {"elem1", "elem2", "elem3", "elem4", "elem5", "elem6", "elem7", "elem8", "elem9"}
+    return render(request, 'index.html', context={'elements': elements})
 
 
 def clear(request):
